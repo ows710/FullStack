329d4cfc2956de86e1832f64ea8a658b81a4f3e4
ows710 <Ohwonseok@git.ajou.ac.kr>
2024-10-01 17:38:54 +0900

    10/1 JSP를 이용한 로그인,회원 정보 조회 및 변경 연구

---
+++ 05_Web/Jsp03_LoginBoard/WebContent/membercontroller.jsp
@@ -0,0 +1,105 @@
+<%@ page language="java" contentType="text/html; charset=UTF-8"
+    pageEncoding="UTF-8"%>
+<% request.setCharacterEncoding("UTF-8"); %>
+<% response.setContentType("text/html; charset=UTF-8"); %>
+
+<%@ page import="com.login.dao.MyMemberDao" %>
+<%@ page import="com.login.dto.MyMemberDto" %>
+<%@ page import="java.util.List" %>
+
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="UTF-8">
+<title>Insert title here</title>
+</head>
+<body>
+<%
+		String command = request.getParameter("command");
+		System.out.println("[command: "+command+"]");
+		
+		MyMemberDao dao = new MyMemberDao();
+
+		if(command.equals("login")){
+			String id = request.getParameter("id");
+			String pw = request.getParameter("pw");
+			
+			MyMemberDto dto = dao.login(id,pw);
+			
+			if(dto.getMyid() != null) {
+				
+				//session
+				session.setAttribute("dto", dto);
+				//default:30분
+				session.setMaxInactiveInterval(60*60);
+				/*
+				page 영역 : 하나의 jsp 페이지 내에서만 객체를 공유하는 영역.
+							   pageContext객체 -> 해당 jsp 페이지 내에서만 유효.
+							   
+				request 영역 : 응답하는 페이지까지 유효한 영역.
+								   setAttribute로 저장하고 getAttribute로 받고.
+								   forward
+				session 영역 : 하나의 브라우저당 1개의 session 객체.
+								   같은 브라우저 내에서 요청되는 페이지들은 같은 객체를 공유.
+
+				application 영역 : 어플리케이션 당 1개의 application 객체가 생성.
+										같은 어플리케이션 내에서 요청되는 모든 페이지들은 같은 객체를 공유.
+				*/
+				
+				
+				if(dto.getMyrole().equals("ADMIN")){
+					response.sendRedirect("adminmain.jsp");
+				}else if(dto.getMyrole().equals("USER")){
+					response.sendRedirect("usermain.jsp");
+				}
+			}else{
+				//로그인 실패시
+				response.sendRedirect("logRes.jsp");
+			}
+			
+		}else if(command.equals("logout")){
+			//로그아웃
+			//session에 담아둔 정보 삭제
+			session.invalidate();
+			response.sendRedirect("index.jsp");
+			
+		}else if(command.equals("userlistall")){
+			//전체 유저 정보 조회
+			List<MyMemberDto> list = dao.selectAll();
+			
+			request.setAttribute("list", list);
+			pageContext.forward("userlistall.jsp");
+		}else if(command.equals("updateroleform")){
+			int myno = Integer.parseInt(request.getParameter("myno"));
+			MyMemberDto dto = dao.selectUser(myno);
+			
+			request.setAttribute("selectone", dto);
+			pageContext.forward("updateroleform.jsp");
+		}else if(command.equals("updaterole")){
+			int myno = Integer.parseInt(request.getParameter("myno"));
+			String myrole = request.getParameter("myrole");
+			
+			int res = dao.updateRole(myno, myrole);
+			
+			if(res > 0){
+				request.setAttribute("msg", "회원등급 조정 성공");
+				request.setAttribute("url", "membercontroller.jsp?command=userlistall");
+			}else {
+				request.setAttribute("msg", "회원등급 조정 실패");
+				request.setAttribute("url", "membercontroller.jsp?command=updateroleform&myno="+myno);
+			}
+			
+			pageContext.forward("result.jsp");
+		}else if(command.equals("userinfo")){
+			int myno = Integer.parseInt(request.getParameter("myno"));
+			MyMemberDto dto = dao.selectUser(myno);
+			
+			request.setAttribute("dto", dto);
+			pageContext.forward("userinfo.jsp");
+			
+			
+		}
+		
+%>
+</body>
+</html>
--- 05_Web/Jsp03_LoginBoard/src/com/login/dao/MyMemberDao.java
+++ 05_Web/Jsp03_LoginBoard/src/com/login/dao/MyMemberDao.java
@@ -1,5 +1,174 @@
 package com.login.dao;
 
-public class MyMemberDao {
+import static common.JDBCTemplate.*;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+
+import com.login.dto.MyMemberDto;
+public class MyMemberDao {
+	
+	//로그인
+	public MyMemberDto login(String id, String pw) {
+		Connection con = getConnection();
+		PreparedStatement pstm = null;
+		ResultSet rs = null;
+		MyMemberDto res = new MyMemberDto();
+		
+		String sql = "	SELECT * FROM MYMEMBER	" +
+						 "	WHERE MYID=? AND MYPW=? AND MYENABLED=?	";
+		
+		try {
+			pstm = con.prepareStatement(sql);
+			pstm.setString(1, id);
+			pstm.setString(2, pw);
+			pstm.setString(3, "Y");
+			System.out.println("03. query 준비: " + sql);
+			
+			rs = pstm.executeQuery();
+			System.out.println("04. query 실행 및 리턴");
+			
+			if(rs.next()) {
+				res.setMyno(rs.getInt(1));
+				res.setMyid(rs.getString(2));
+				res.setMypw(rs.getString(3));
+				res.setMyname(rs.getString(4));
+				res.setMyaddr(rs.getString(5));
+				res.setMyphone(rs.getString(6));
+				res.setMyemail(rs.getString(7));
+				res.setMyenabled(rs.getString(8));
+				res.setMyrole(rs.getString(9));
+			}
+			
+		} catch (SQLException e) {
+			System.out.println("3/4 단계 오류");
+			e.printStackTrace();
+		} finally {
+			close(rs);
+			close(pstm);
+			close(con);
+			System.out.println("05. db 종료\n");
+		}
+		return res;
+	}
+	
+	
+	
+	//전체 회원 정보 조회
+	public List<MyMemberDto> selectAll(){
+		Connection con = getConnection();
+		PreparedStatement pstm = null;
+		ResultSet rs = null;
+		List<MyMemberDto> res = new ArrayList<MyMemberDto>();
+		
+		String sql = " SELECT * FROM MYMEMBER ORDER BY MYNO DESC ";
+		
+		try {
+			pstm = con.prepareStatement(sql);
+			System.out.println("03. query 준비: " + sql);
+			
+			rs = pstm.executeQuery();
+			System.out.println("04. query 실행 및 리턴");
+			
+			while(rs.next()) {
+				MyMemberDto tmp = new MyMemberDto();
+				tmp.setMyno(rs.getInt(1));
+				tmp.setMyid(rs.getString(2));
+				tmp.setMypw(rs.getString(3));
+				tmp.setMyname(rs.getString(4));
+				tmp.setMyaddr(rs.getString(5));
+				tmp.setMyphone(rs.getString(6));
+				tmp.setMyemail(rs.getString(7));
+				tmp.setMyenabled(rs.getString(8));
+				tmp.setMyrole(rs.getString(9));
+				res.add(tmp);
+			}
+			
+		} catch (SQLException e) {
+			System.out.println("3/4 단계 오류");
+			e.printStackTrace();
+		} finally {
+			close(rs);
+			close(pstm);
+			close(con);
+			System.out.println("05. db 종료\n");
+		}
+		return res;
+	}
+	
+	
+	//회원 정보 조회
+	public MyMemberDto selectUser(int myno) {
+		Connection con = getConnection();
+		PreparedStatement pstm = null;
+		ResultSet rs = null;
+		MyMemberDto res = null;
+		
+		String sql = " SELECT * FROM MYMEMBER WHERE MYNO=? ";
+		
+		try {
+			pstm = con.prepareStatement(sql);
+			pstm.setInt(1, myno);
+			System.out.println("03. query 준비: " + sql);
+			
+			rs = pstm.executeQuery();
+			System.out.println("04. query 실행 및 리턴");
+			
+			if(rs.next()) {
+				res = new MyMemberDto();
+				res.setMyno(rs.getInt(1));
+				res.setMyid(rs.getString(2));
+				res.setMypw(rs.getString(3));
+				res.setMyname(rs.getString(4));
+				res.setMyaddr(rs.getString(5));
+				res.setMyphone(rs.getString(6));
+				res.setMyemail(rs.getString(7));
+				res.setMyenabled(rs.getString(8));
+				res.setMyrole(rs.getString(9));
+			}
+			
+		} catch (SQLException e) {
+			System.out.println("3/4 단계 오류");
+			e.printStackTrace();
+		} finally {
+			close(rs);
+			close(pstm);
+			close(con);
+			System.out.println("05. db 종료\n");
+		}
+		return res;
+	}
+	
+	
+	//회원등급변경
+	public int updateRole(int myno, String myrole) {
+		Connection con = getConnection();
+		PreparedStatement pstm = null;
+		int res = 0;
+		
+		String sql = " UPDATE MYMEMBER SET MYROLE=? WHERE MYNO=? ";
+		
+		try {
+			pstm = con.prepareStatement(sql);
+			pstm.setString(1, myrole);
+			pstm.setInt(2,  myno);
+			System.out.println("03. query 준비: " + sql);
+			
+			res = pstm.executeUpdate();
+			System.out.println("04. query 실행 및 리턴");
+			
+		} catch (SQLException e) {
+			System.out.println("3/4 단계 오류");
+			e.printStackTrace();
+		} finally {
+			close(pstm);
+			close(con);
+			System.out.println("05. db 종료\n");
+		}
+		return res;
+	}
 }
