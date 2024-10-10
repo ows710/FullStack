4ca40fe0969b1e2aef65089f573f59c8baadab2a
ows710 <Ohwonseok@git.ajou.ac.kr>
2024-10-10 18:05:43 +0900

    10/10 JSTL 관련 MVC패턴(게시물 수정,삭제) 및 댓글조회 연구

---
+++ 05_Web/Jsp08_AnswerBoard/src/com/answer/controller/AnswerController.java
@@ -0,0 +1,46 @@
+package com.answer.controller;
+
+import java.io.IOException;
+import java.util.List;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import com.answer.dao.AnswerDao;
+import com.answer.dto.AnswerDto;
+
+@WebServlet("/answer.do")
+public class AnswerController extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		request.setCharacterEncoding("UTF-8");
+		response.setContentType("text/html; charset=UTF-8");
+		
+		String command = request.getParameter("command");
+		System.out.println("{"+command+"}");
+		
+		AnswerDao dao = new AnswerDao();
+		
+		if(command.equals("list")) {
+			List<AnswerDto> list = dao.selectAll();
+			request.setAttribute("list", list);
+			RequestDispatcher dis = request.getRequestDispatcher("boardlist.jsp");
+			dis.forward(request, response);
+			
+		}else if(command.equals("writeform")) {
+			response.sendRedirect("boardwrite.jsp");
+			
+		}
+		
+	}
+
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		doGet(request, response);
+	}
+
+}
