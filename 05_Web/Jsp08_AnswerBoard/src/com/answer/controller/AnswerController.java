ca56f3674ec4b8b7c4fe5d5ae993ef8feb4cd134
ows710 <Ohwonseok@git.ajou.ac.kr>
2024-10-11 16:39:47 +0900

    10/11 JSTL을 이용한 댓글 수정,삭제 및 대댓글 작성 연구

--- 05_Web/Jsp08_AnswerBoard/src/com/answer/controller/AnswerController.java
+++ 05_Web/Jsp08_AnswerBoard/src/com/answer/controller/AnswerController.java
@@ -34,8 +34,114 @@ public class AnswerController extends HttpServlet {
 			
 		}else if(command.equals("writeform")) {
 			response.sendRedirect("boardwrite.jsp");
+		}else if(command.equals("boardwrite")) {
+			String title = request.getParameter("title");
+			String content = request.getParameter("content");
+			String writer = request.getParameter("writer");
+			
+			AnswerDto dto = new AnswerDto();
+			dto.setTitle(title);
+			dto.setContent(content);
+			dto.setWriter(writer);
+			
+			int res = dao.insert(dto);
+			
+			if(res > 0) {
+				request.setAttribute("msg", "글 작성 성공");
+				request.setAttribute("url", "answer.do?command=list");
+			}else {
+				request.setAttribute("msg", "글 작성 실패");
+				request.setAttribute("url", "answer.do?command=writeform");
+			}
+			
+			RequestDispatcher dis = request.getRequestDispatcher("result.jsp");
+			dis.forward(request, response);
+			
+		}else if(command.equals("detail")) {
+			int boardno = Integer.parseInt(request.getParameter("boardno"));
+			
+			AnswerDto dto = dao.selectOne(boardno);
+			
+			request.setAttribute("dto", dto);
+			RequestDispatcher dis = request.getRequestDispatcher("boarddetail.jsp");
+			dis.forward(request, response);
+		}else if(command.equals("updateform")) {
+			int boardno = Integer.parseInt(request.getParameter("boardno"));
+			
+			AnswerDto dto = dao.selectOne(boardno);
+			request.setAttribute("dto", dto);
+			RequestDispatcher dis = request.getRequestDispatcher("boardupdate.jsp");
+			dis.forward(request, response);
+		}else if(command.equals("boardupdate")) {
+			int boardno = Integer.parseInt(request.getParameter("boardno"));
+			String title = request.getParameter("title");
+			String content = request.getParameter("content");
+			
+			AnswerDto dto = new AnswerDto();
+			dto.setBoardno(boardno);
+			dto.setTitle(title);
+			dto.setContent(content);
+			
+			int res = dao.update(dto);
+			
+			if(res > 0) {
+				request.setAttribute("msg", "글 수정 성공");
+				request.setAttribute("url", "answer.do?command=detail&boardno="+boardno);
+			}else {
+				request.setAttribute("msg", "글 수정 실패");
+				request.setAttribute("url", "answer.do?command=updateform&boardno="+boardno);
+			}
+			
+			RequestDispatcher dis = request.getRequestDispatcher("result.jsp");
+			dis.forward(request, response);
+			
+		}else if(command.equals("answerform")) {
+			int parentboardno = Integer.parseInt(request.getParameter("parentboardno"));
+			
+			AnswerDto dto = dao.selectOne(parentboardno);
+			
+			request.setAttribute("parent", dto);
+			RequestDispatcher dis = request.getRequestDispatcher("answerwrite.jsp");
+			dis.forward(request, response);
+			
+			
+		}else if(command.equals("answerwrite")) {
+			int parentboardno = Integer.parseInt(request.getParameter("parentboardno"));
+			
+			String title = request.getParameter("title");
+			String content = request.getParameter("content");
+			String writer = request.getParameter("writer");
+			
+			//update groupsq
+			AnswerDto parent = dao.selectOne(parentboardno);
+			
+			int parentgroupno = parent.getGroupno();
+			int parentgroupsq = parent.getGroupsq();
+			int parenttitletab = parent.getTitletab();
+			
+			int updateRes = dao.updateAnswer(parentgroupno, parentgroupsq);
+			if(updateRes > 0) {
+				System.out.println("순서변경성공");
+			}else {
+				System.out.println("순서 변경 실패 or 변경할 글 없음");
+			}
+			//insert
+			AnswerDto dto = new AnswerDto(0, parentgroupno, parentgroupsq+1, parenttitletab+2,
+										   title, content, writer, null);
+			int res = dao.insertAnswer(dto);
+			
+			if(res > 0) {
+				request.setAttribute("msg", "답변글 작성 완료");
+				request.setAttribute("url", "answer.do?command=list");
+			}else {
+				request.setAttribute("msg", "답변글 작성 실패");
+				request.setAttribute("url", "answer.do?command=detail&boardno="+parentboardno);
+			}
+			RequestDispatcher dis = request.getRequestDispatcher("result.jsp");
+			dis.forward(request, response);
 			
 		}
+		
 		
 	}
 
--- 05_Web/Jsp08_AnswerBoard/src/com/answer/dao/AnswerDao.java
+++ 05_Web/Jsp08_AnswerBoard/src/com/answer/dao/AnswerDao.java
@@ -45,7 +45,7 @@ public class AnswerDao {
 		} catch (SQLException e) {
 			System.out.println("3/4 단계 오류");
 			e.printStackTrace();
-		}finally {
+		} finally {
 			close(rs);
 			close(pstm);
 			close(con);
@@ -53,4 +53,184 @@ public class AnswerDao {
 		}
 		return res;
 	}
+	
+	public int insert(AnswerDto dto) {
+		Connection con = getConnection();
+		PreparedStatement pstm = null;
+		int res = 0;
+		
+		String sql = " INSERT INTO ANSWERBOARD VALUES(BOARDNOSQ.NEXTVAL, "+
+							"GROUPNOSQ.NEXTVAL, 1, 0, ?, ?, ?, CURRENT_DATE) ";
+		
+		try {
+			pstm = con.prepareStatement(sql);
+			pstm.setString(1,  dto.getTitle());
+			pstm.setString(2, dto.getContent());
+			pstm.setString(3,  dto.getWriter());
+			System.out.println("03. query 준비: " + sql);
+			
+			res = pstm.executeUpdate();
+			System.out.println("04. query 실행 및 리턴");
+			
+			if(res > 0) {
+				commit(con);
+			}	
+			
+		} catch (SQLException e) {
+			System.out.println("3/4 단계 에러");
+			e.printStackTrace();
+		} finally {
+			close(pstm);
+			close(con);
+			System.out.println("05. db 종료\n");
+		}
+		return res;
+	}
+	
+	
+	public AnswerDto selectOne(int boardno) {
+		Connection con = getConnection();
+		PreparedStatement pstm = null;
+		ResultSet rs = null;
+		AnswerDto res = new AnswerDto();
+		
+		String sql = " SELECT * FROM ANSWERBOARD WHERE BOARDNO=? ";
+		
+		try {
+			pstm = con.prepareStatement(sql);
+			pstm.setInt(1,  boardno);
+			System.out.println("03. query 준비: " + sql);
+			
+			rs = pstm.executeQuery();
+			System.out.println("04. query 실행 및 리턴");
+			if(rs.next()) {
+				res.setBoardno(rs.getInt(1));
+				res.setGroupno(rs.getInt(2));
+				res.setGroupsq(rs.getInt(3));
+				res.setTitletab(rs.getInt(4));
+				res.setTitle(rs.getString(5));
+				res.setContent(rs.getString(6));
+				res.setWriter(rs.getString(7));
+				res.setDate(rs.getDate(8));
+			}
+		} catch (SQLException e) {
+			System.out.println("3/4 단계 오류");
+			e.printStackTrace();
+		} finally {
+			close(rs);
+			close(pstm);
+			close(con);
+			System.out.println("05. db 종료\n");
+		}
+		return res;
+	}
+	
+	
+	public int update(AnswerDto dto) {
+		Connection con = getConnection();
+		PreparedStatement pstm = null;
+		int res = 0;
+		
+		String sql = " UPDATE ANSWERBOARD SET TITLE=?, CONTENT=? WHERE BOARDNO=? ";
+		
+		try {
+			pstm = con.prepareStatement(sql);
+			pstm.setString(1,  dto.getTitle());
+			pstm.setString(2, dto.getContent());
+			pstm.setInt(3,  dto.getBoardno());
+			System.out.println("03. query 준비: " + sql);
+			
+			res = pstm.executeUpdate();
+			System.out.println("04. query 실행 및 리턴");
+			
+			if(res > 0) {
+				commit(con);
+			}
+			
+		} catch (SQLException e) {
+			System.out.println("3/4 단계 오류");
+			e.printStackTrace();
+		} finally {
+			close(pstm);
+			close(con);
+			System.out.println("05. db 종료\n");
+		}
+		return res;
+	}
+	
+	
+	
+	public int updateAnswer(int groupno, int groupsq) {
+		Connection con = getConnection();
+		PreparedStatement pstm = null;
+		int res = 0;
+		
+		/* UPDATE ANSWERBOARD SET GROUPSQ = GROUPSQ+1
+			WHERE GROUPNO = (SELECT GROUPNO FROM ANSWERBOARD WHERE BOARDNO=2)
+				AND GROUPSQ>(SELECT GROUPSQ FROM ANSWERBOARD WHERE BOARDNO=2);
+		*/
+		String sql = " UPDATE ANSWERBOARD SET GROUPSQ=GROUPSQ+1 " + 
+						 " WHERE GROUPNO = ? " +
+						 "	    AND GROUPSQ > ? ";
+		
+		try {
+			pstm = con.prepareStatement(sql);
+			pstm.setInt(1,  groupno);
+			pstm.setInt(2, groupsq);
+			System.out.println("03. query 준비: " + sql);
+			
+			res = pstm.executeUpdate();
+			System.out.println("04. query 실행 및 리턴");
+			
+			if(res > 0) {
+				commit(con);
+			}
+		} catch (SQLException e) {
+			System.out.println("3/4 단계 오류");
+			e.printStackTrace();
+		} finally {
+			close(pstm);
+			close(con);
+			System.out.println("05. db 종료\n");
+		}
+		return res;
+	}
+	
+	
+	public int insertAnswer(AnswerDto dto) {
+		Connection con = getConnection();
+		PreparedStatement pstm = null;
+		int res = 0;
+		
+		String sql = " INSERT INTO ANSWERBOARD VALUES(BOARDNOSQ.NEXTVAL, ?,?,?,?,?,?, CURRENT_DATE) ";
+		
+		try {
+			pstm= con.prepareStatement(sql);
+			pstm.setInt(1, dto.getGroupno());
+			pstm.setInt(2, dto.getGroupsq());
+			pstm.setInt(3, dto.getTitletab());
+			pstm.setString(4, dto.getTitle());
+			pstm.setString(5, dto.getContent());
+			pstm.setString(6, dto.getWriter());
+			System.out.println("03. query 준비: " + sql);
+			
+			res = pstm.executeUpdate();
+			System.out.println("04. query 실행 및 리턴");
+			
+			if(res > 0) {
+				commit(con);
+			}
+		} catch (SQLException e) {
+			System.out.println("3/4 단계 오류");
+			e.printStackTrace();
+		} finally {
+			close(pstm);
+			close(con);
+			System.out.println("05. db 종료\n");
+		}
+		
+		return res;
+	}
+	
+	
 }
